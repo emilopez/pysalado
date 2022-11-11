597fdabbe171e6370a7efa7cf75104c7a68322fa
Emiliano <emiliano.lopez@gmail.com>
2022-11-11 17:13:49 -0300

    streamliet 1.0.0 and icons

--- to_build/src/app.py
+++ to_build/src/app.py
@@ -1,9 +1,13 @@
-import streamlit as st
-import plotly.graph_objects as go
+# Autor: Emiliano Lopez <emiliano.lopez@gmail.com>
+# Programa: PySalado v1.0.0
+# Fecha: 11/11/2022
+
+import os
+import datetime
 import numpy as np
 import pandas as pd
-import datetime
-import os
+import streamlit as st
+import plotly.graph_objects as go
 
 # --- funciones ---
 
@@ -195,7 +199,8 @@ st.set_page_config(
      page_title="Pronóstico Río Salado",
      page_icon="🇦🇷",
      layout="wide",
-     initial_sidebar_state="collapsed",
+     initial_sidebar_state="expanded",
+     #initial_sidebar_state="collapsed",
 )
 st.markdown(""" <style>
 #MainMenu {visibility: hidden;}
@@ -208,6 +213,13 @@ mapbox_access_token = "pk.eyJ1IjoiZW1pbG9wZXoiLCJhIjoiY2swMTE1dTR0MHM4MDNkcDdwaj
 st.header("Río Salado - Sistema de Alerta Hidrológico - Pronósticos")
 
 with st.sidebar:
+    st.header("Acerca de PySalado")
+    st.write("Programa:", "PySalado")
+    st.write("Versión:",1.0)
+    st.write("Fecha:", "11/11/2022")
+    st.write("Autor:", "Mg. Ing. Emiliano P. López")
+    st.write("Email:", "emiliano.lopez@gmail.com")
+
     st.header("Cotas Estaciones")
     ceroRP39   = st.number_input('Cero RP39',    value = 36.97) # Paso Las Piedras.
     ceroRP02   = st.number_input('Cero RP02',    value = 22.96) # San Justo
@@ -219,156 +231,145 @@ with st.sidebar:
     ceroRP262  = st.number_input('Cero RP262',   value = 33.15) # Afluente Arroyo San Antonio / Petronilla
     ceroRP50S  = st.number_input('Cero RP50S',   value = 28.05) # Arroyo Cululu / Cululu
 
-tabs = st.tabs(["Principal", "Acerca de"])
 
-principal = tabs[0]
-acerca_de = tabs[1]
-with principal:
-    c1, c2, c3, c4 = st.columns([1.7,0.3,2.1,2.1])
+c1, c2, c3, c4 = st.columns([1.7,0.3,2.1,2.1])
 
-    uploaded_file = c1.file_uploader("Cargar archivo CSV [datos telemétricos]", type=["csv"])
-    desde_n_dias = c2.selectbox("Días previos", (7, 50, 100))
-        
+uploaded_file = c1.file_uploader("Cargar archivo CSV [datos telemétricos]", type=["csv"])
+desde_n_dias = c2.selectbox("Días previos", (7, 50, 100))
+    
 
-    if uploaded_file is not None:
-        
-        # lectura archivo CSV
-        data_altura_rios = pd.read_csv(uploaded_file, sep=";", parse_dates=["Fecha"], decimal = ',', comment="#", dayfirst=True)
-        data_altura_rios = data_altura_rios[-desde_n_dias:]
-        
-        # trazas de alturas observadas telemetricamente
-        fig = go.Figure()
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP70 + data_altura_rios.RP70, mode="markers+lines", name="Ruta 70",line=dict(color="dodgerblue")))
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP62 + data_altura_rios.RP62, mode="markers+lines", name="Ruta 62",line=dict(color="salmon")))
+if uploaded_file is not None:
+    
+    # lectura archivo CSV
+    data_altura_rios = pd.read_csv(uploaded_file, sep=";", parse_dates=["Fecha"], decimal = ',', comment="#", dayfirst=True)
+    data_altura_rios = data_altura_rios[-desde_n_dias:]
+    
+    # trazas de alturas observadas telemetricamente
+    fig = go.Figure()
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP70 + data_altura_rios.RP70, mode="markers+lines", name="Ruta 70",line=dict(color="dodgerblue")))
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP62 + data_altura_rios.RP62, mode="markers+lines", name="Ruta 62",line=dict(color="salmon")))
 
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP39 + data_altura_rios.RP39, mode="markers+lines", name="RP 39",line=dict(color="navy")))
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP02 + data_altura_rios.RP02, mode="markers+lines", name="RP 02",line=dict(color="olive")))
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP04 + data_altura_rios.RP04, mode="markers+lines", name="RP 04",line=dict(color="chocolate")))
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRN11 + data_altura_rios.RN11, mode="markers+lines", name="RN 11",line=dict(color="lightgreen")))
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroPTOSFE + data_altura_rios.PTOSFE, mode="markers+lines", name="Puerto SFe",line=dict(color="lightseagreen")))
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP262 + data_altura_rios.RP262, mode="markers+lines", name="RP 262",line=dict(color="lightslategrey")))
-        fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP50S + data_altura_rios.RP50S, mode="markers+lines", name="RP 50S",line=dict(color="mediumvioletred")))
-        
-        # Pronosticos
-        # último día de mediciones
-        last_date = pd.to_datetime(data_altura_rios.Fecha.values[-1])
-        
-        # 3 días de pronóstico para RP70
-        tres = 3
-        x_prono_RP70 = [last_date + datetime.timedelta(days=dia) for dia in range(1,tres+1)]
-        yRP70_prono = np.array(get_prono_R70(data_altura_rios))
-        
-        # 2 dias de pronostico para RP62
-        dos = 2
-        x_prono_RP62 = [last_date + datetime.timedelta(days=dia) for dia in range(1,dos+1)]
-        yRP62_prono = np.array(get_prono_R62(data_altura_rios))
-        
-        # trazas de pronosticos
-        fig.add_trace(go.Scattergl(x=x_prono_RP70, y=ceroRP70 + yRP70_prono, mode="markers+lines", name="Prono R70",line=dict(color="dodgerblue", width=3, dash='dot')))
-        fig.add_trace(go.Scattergl(x=x_prono_RP62, y=ceroRP62 + yRP62_prono, mode="markers+lines", name="Prono R62",line=dict(color="salmon", width=3, dash='dot')))
-        
-        # DATAFRAMES con datos de pronostico
-        values_prono_R70 = pd.DataFrame({"Día":[1, 2, 3], "Fecha":x_prono_RP70, "Altura h [m]": yRP70_prono, "Nivel H [m IGN]": ceroRP70 + yRP70_prono})
-        values_prono_R70.set_index("Día")
-        values_prono_R70["RMSE"] = pd.Series(RMSE_R70)
-        values_prono_R70["H-RMSE"] = values_prono_R70["Nivel H [m IGN]"] - values_prono_R70["RMSE"]
-        values_prono_R70["H+RMSE"] = values_prono_R70["Nivel H [m IGN]"] + values_prono_R70["RMSE"]
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP39 + data_altura_rios.RP39, mode="markers+lines", name="RP 39",line=dict(color="navy")))
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP02 + data_altura_rios.RP02, mode="markers+lines", name="RP 02",line=dict(color="olive")))
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP04 + data_altura_rios.RP04, mode="markers+lines", name="RP 04",line=dict(color="chocolate")))
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRN11 + data_altura_rios.RN11, mode="markers+lines", name="RN 11",line=dict(color="lightgreen")))
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroPTOSFE + data_altura_rios.PTOSFE, mode="markers+lines", name="Puerto SFe",line=dict(color="lightseagreen")))
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP262 + data_altura_rios.RP262, mode="markers+lines", name="RP 262",line=dict(color="lightslategrey")))
+    fig.add_trace(go.Scattergl(x=data_altura_rios.Fecha, y = ceroRP50S + data_altura_rios.RP50S, mode="markers+lines", name="RP 50S",line=dict(color="mediumvioletred")))
+    
+    # Pronosticos
+    # último día de mediciones
+    last_date = pd.to_datetime(data_altura_rios.Fecha.values[-1])
+    
+    # 3 días de pronóstico para RP70
+    tres = 3
+    x_prono_RP70 = [last_date + datetime.timedelta(days=dia) for dia in range(1,tres+1)]
+    yRP70_prono = np.array(get_prono_R70(data_altura_rios))
+    
+    # 2 dias de pronostico para RP62
+    dos = 2
+    x_prono_RP62 = [last_date + datetime.timedelta(days=dia) for dia in range(1,dos+1)]
+    yRP62_prono = np.array(get_prono_R62(data_altura_rios))
+    
+    # trazas de pronosticos
+    fig.add_trace(go.Scattergl(x=x_prono_RP70, y=ceroRP70 + yRP70_prono, mode="markers+lines", name="Prono R70",line=dict(color="dodgerblue", width=3, dash='dot')))
+    fig.add_trace(go.Scattergl(x=x_prono_RP62, y=ceroRP62 + yRP62_prono, mode="markers+lines", name="Prono R62",line=dict(color="salmon", width=3, dash='dot')))
+    
+    # DATAFRAMES con datos de pronostico
+    values_prono_R70 = pd.DataFrame({"Día":[1, 2, 3], "Fecha":x_prono_RP70, "Altura h [m]": yRP70_prono, "Nivel H [m IGN]": ceroRP70 + yRP70_prono})
+    values_prono_R70.set_index("Día")
+    values_prono_R70["RMSE"] = pd.Series(RMSE_R70)
+    values_prono_R70["H-RMSE"] = values_prono_R70["Nivel H [m IGN]"] - values_prono_R70["RMSE"]
+    values_prono_R70["H+RMSE"] = values_prono_R70["Nivel H [m IGN]"] + values_prono_R70["RMSE"]
 
-        values_prono_R62 = pd.DataFrame({"Día":[1, 2],"Fecha":x_prono_RP62, "Altura h [m]": yRP62_prono,"Nivel H [m IGN]": ceroRP62 + yRP62_prono})
-        values_prono_R62.set_index("Día")
-        values_prono_R62["RMSE"] = pd.Series(RMSE_R62)
-        values_prono_R62["H-RMSE"] = values_prono_R62["Nivel H [m IGN]"] - values_prono_R62["RMSE"]
-        values_prono_R62["H+RMSE"] = values_prono_R62["Nivel H [m IGN]"] + values_prono_R62["RMSE"]
+    values_prono_R62 = pd.DataFrame({"Día":[1, 2],"Fecha":x_prono_RP62, "Altura h [m]": yRP62_prono,"Nivel H [m IGN]": ceroRP62 + yRP62_prono})
+    values_prono_R62.set_index("Día")
+    values_prono_R62["RMSE"] = pd.Series(RMSE_R62)
+    values_prono_R62["H-RMSE"] = values_prono_R62["Nivel H [m IGN]"] - values_prono_R62["RMSE"]
+    values_prono_R62["H+RMSE"] = values_prono_R62["Nivel H [m IGN]"] + values_prono_R62["RMSE"]
 
-        # Grafico bandas de error
-        #  RP70
-        fig.add_trace(go.Scatter(name='Banda superior', x = values_prono_R70['Fecha'], y = values_prono_R70["H+RMSE"],
-            mode='lines', marker=dict(color="#444"), line=dict(width=0), showlegend=False),
-        )
-        fig.add_trace(go.Scatter(name='Banda inferior', x=values_prono_R70['Fecha'], y=values_prono_R70["H-RMSE"],
-            marker=dict(color="#444"), line=dict(width=0), mode='lines', fillcolor='rgba(0, 88, 20, 0.4)', fill='tonexty', showlegend=False)
-        )
-        #  RP62
-        fig.add_trace(go.Scatter(name='Banda superior', x = values_prono_R62['Fecha'], y = values_prono_R62["H+RMSE"],
-            mode='lines', marker=dict(color="#444"), line=dict(width=0), showlegend=False),
-        )
-        fig.add_trace(go.Scatter(name='Banda inferior', x=values_prono_R62['Fecha'], y=values_prono_R62["H-RMSE"],
-            marker=dict(color="#444"), line=dict(width=0), mode='lines', fillcolor='rgba(55, 0, 20, 0.4)', fill='tonexty', showlegend=False)
-        )
+    # Grafico bandas de error
+    #  RP70
+    fig.add_trace(go.Scatter(name='Banda superior', x = values_prono_R70['Fecha'], y = values_prono_R70["H+RMSE"],
+        mode='lines', marker=dict(color="#444"), line=dict(width=0), showlegend=False),
+    )
+    fig.add_trace(go.Scatter(name='Banda inferior', x=values_prono_R70['Fecha'], y=values_prono_R70["H-RMSE"],
+        marker=dict(color="#444"), line=dict(width=0), mode='lines', fillcolor='rgba(0, 88, 20, 0.4)', fill='tonexty', showlegend=False)
+    )
+    #  RP62
+    fig.add_trace(go.Scatter(name='Banda superior', x = values_prono_R62['Fecha'], y = values_prono_R62["H+RMSE"],
+        mode='lines', marker=dict(color="#444"), line=dict(width=0), showlegend=False),
+    )
+    fig.add_trace(go.Scatter(name='Banda inferior', x=values_prono_R62['Fecha'], y=values_prono_R62["H-RMSE"],
+        marker=dict(color="#444"), line=dict(width=0), mode='lines', fillcolor='rgba(55, 0, 20, 0.4)', fill='tonexty', showlegend=False)
+    )
 
-        fig.update_layout(
-            height =630,
-            font  = dict(family = "Calibri", size = 20,),
-            xaxis = dict(title = "Fecha", showline = True, showgrid = True, tickformat="%d/%m/%Y",tickfont = dict(family = 'Calibri')),
-            yaxis = dict(title = "Nivel [m IGN]", showline = True, showgrid = True,tickfont = dict(family = 'Calibri')),
-            legend = dict(orientation="h",yanchor="bottom", y=1),
-            margin ={'l':0,'t':100,'b':0,'r':0},
-            hovermode="x"
-        )
-        # show
-        values_prono_R70['Fecha'] = pd.to_datetime(values_prono_R70['Fecha']).dt.strftime('%d/%m/%Y')
-        values_prono_R62['Fecha'] = pd.to_datetime(values_prono_R62['Fecha']).dt.strftime('%d/%m/%Y')
-        # CSS to inject contained in a string
-        hide_table_row_index = """
-                    <style>
-                    thead tr th:first-child {display:none}
-                    tbody th {display:none}
-                    </style>
-                    """
+    fig.update_layout(
+        height =630,
+        font  = dict(family = "Calibri", size = 20,),
+        xaxis = dict(title = "Fecha", showline = True, showgrid = True, tickformat="%d/%m/%Y",tickfont = dict(family = 'Calibri')),
+        yaxis = dict(title = "Nivel [m IGN]", showline = True, showgrid = True,tickfont = dict(family = 'Calibri')),
+        legend = dict(orientation="h",yanchor="bottom", y=1),
+        margin ={'l':0,'t':100,'b':0,'r':0},
+        hovermode="x"
+    )
+    # show
+    values_prono_R70['Fecha'] = pd.to_datetime(values_prono_R70['Fecha']).dt.strftime('%d/%m/%Y')
+    values_prono_R62['Fecha'] = pd.to_datetime(values_prono_R62['Fecha']).dt.strftime('%d/%m/%Y')
+    # CSS to inject contained in a string
+    hide_table_row_index = """
+                <style>
+                thead tr th:first-child {display:none}
+                tbody th {display:none}
+                </style>
+                """
 
-        # Inject CSS with Markdown
-        st.markdown(hide_table_row_index, unsafe_allow_html=True)
+    # Inject CSS with Markdown
+    st.markdown(hide_table_row_index, unsafe_allow_html=True)
 
-        with c3.expander("Valores Pronosticados RP70"):
-            st.table(values_prono_R70.style.format({"Altura h [m]": "{:.2f}","Nivel H [m IGN]": "{:.2f}", "RMSE":"{:.2f}", "H-RMSE":"{:.2f}", "H+RMSE":"{:.2f}"}))
-        with c4.expander("Valores Pronosticados RP62"):
-            st.table(values_prono_R62.style.format({"Altura h [m]": "{:.2f}","Nivel H [m IGN]": "{:.2f}", "RMSE":"{:.2f}", "H-RMSE":"{:.2f}", "H+RMSE":"{:.2f}"}))
+    with c3.expander("Valores Pronosticados RP70"):
+        st.table(values_prono_R70.style.format({"Altura h [m]": "{:.2f}","Nivel H [m IGN]": "{:.2f}", "RMSE":"{:.2f}", "H-RMSE":"{:.2f}", "H+RMSE":"{:.2f}"}))
+    with c4.expander("Valores Pronosticados RP62"):
+        st.table(values_prono_R62.style.format({"Altura h [m]": "{:.2f}","Nivel H [m IGN]": "{:.2f}", "RMSE":"{:.2f}", "H-RMSE":"{:.2f}", "H+RMSE":"{:.2f}"}))
 
-        c1, c2 = st.columns([2, 1])
-        c1.plotly_chart(fig, use_container_width=True)
-        with c1.expander("Ver datos telemétricos"):
-            st.write(data_altura_rios)
+    c1, c2 = st.columns([2, 1])
+    c1.plotly_chart(fig, use_container_width=True)
+    with c1.expander("Ver datos telemétricos"):
+        st.write(data_altura_rios)
 
-        #---------------------------------------
-        #       COLUMNA 2
-        #---------------------------------------
-        # LEO archivo de metadatos de estaciones
-        meta_estaciones_sah = os.path.join(os.path.dirname(__file__), "datos", "meta_estaciones_sah.csv")
-        estaciones_sah_df = pd.read_csv(meta_estaciones_sah, sep=";", decimal = ',')
+    #---------------------------------------
+    #       COLUMNA 2
+    #---------------------------------------
+    # LEO archivo de metadatos de estaciones
+    meta_estaciones_sah = os.path.join(os.path.dirname(__file__), "datos", "meta_estaciones_sah.csv")
+    estaciones_sah_df = pd.read_csv(meta_estaciones_sah, sep=";", decimal = ',')
 
-        # agrega al dataframe ùltimas lecturas telemétricas a las estaciones del modelo
-        estaciones_sah_df = add_data_to_metadata(estaciones_sah_df, data_altura_rios)
+    # agrega al dataframe ùltimas lecturas telemétricas a las estaciones del modelo
+    estaciones_sah_df = add_data_to_metadata(estaciones_sah_df, data_altura_rios)
 
-        # creo mapa plotly
-        estaciones_sah_map = go.Scattermapbox(mode = "markers", lon = estaciones_sah_df["lng"], lat = estaciones_sah_df["lat"], marker = {'size': 10}, name = "",
-                                            hovertemplate =   "<b>" + estaciones_sah_df["Nombre"] + "</b><br><br>" +
-                                                            "Coord: %{lon},%{lat}<br>"+
-                                                            "Río: " + estaciones_sah_df["Rio"] + "<br>" +
-                                                            "Ruta: "+ estaciones_sah_df["Ruta"] +"<br>" +
-                                                            "Descripción:" + estaciones_sah_df["Descripción"] + "<br><br>" + 
-                                                            "Último dato (Altura h [m]): " + estaciones_sah_df["ultimo_dato"] + "<br>" + 
-                                                            "Fecha: " + estaciones_sah_df["fecha_ultimo_dato"] 
-                                            )
-        layout = go.Layout(
-            #title = "Estaciones SAH",
-            title_x=0.5,
-            title_y=0.95,
-            width=400, height=630, 
-            margin ={'l':0,'t':50,'b':0,'r':0},
-            mapbox = {
-                'accesstoken':mapbox_access_token,
-                'center': {'lat': -30.3, 'lon': -61},
-                'style': "satellite-streets",
-                'zoom': 7})
-        figure = go.Figure(data=[estaciones_sah_map], layout=layout)
-        # show mapa 
-        c2.plotly_chart(figure, use_container_width=True)
-        with c2.expander("Ver datos estaciones"):
-            st.write(estaciones_sah_df)
-with acerca_de:
-    st.write("Programa:", "PySalado")
-    st.write("Versión:",1.0)
-    st.write("Dependencias:", "Python 3.8.5, Pandas, Numpy, Plotly")
-    st.write("Fecha:", "08/11/2022")
-    st.write("Autor:", "Mg. Ing. Emiliano P. López")
-    st.write("Email:", "emiliano.lopez@gmail.com")
+    # creo mapa plotly
+    estaciones_sah_map = go.Scattermapbox(mode = "markers", lon = estaciones_sah_df["lng"], lat = estaciones_sah_df["lat"], marker = {'size': 10}, name = "",
+                                        hovertemplate =   "<b>" + estaciones_sah_df["Nombre"] + "</b><br><br>" +
+                                                        "Coord: %{lon},%{lat}<br>"+
+                                                        "Río: " + estaciones_sah_df["Rio"] + "<br>" +
+                                                        "Ruta: "+ estaciones_sah_df["Ruta"] +"<br>" +
+                                                        "Descripción:" + estaciones_sah_df["Descripción"] + "<br><br>" + 
+                                                        "Último dato (Altura h [m]): " + estaciones_sah_df["ultimo_dato"] + "<br>" + 
+                                                        "Fecha: " + estaciones_sah_df["fecha_ultimo_dato"] 
+                                        )
+    layout = go.Layout(
+        #title = "Estaciones SAH",
+        title_x=0.5,
+        title_y=0.95,
+        width=400, height=630, 
+        margin ={'l':0,'t':50,'b':0,'r':0},
+        mapbox = {
+            'accesstoken':mapbox_access_token,
+            'center': {'lat': -30.3, 'lon': -61},
+            'style': "satellite-streets",
+            'zoom': 7})
+    figure = go.Figure(data=[estaciones_sah_map], layout=layout)
+    # show mapa 
+    c2.plotly_chart(figure, use_container_width=True)
+    with c2.expander("Ver datos estaciones"):
+        st.write(estaciones_sah_df)
